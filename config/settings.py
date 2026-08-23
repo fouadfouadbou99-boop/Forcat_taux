24bcaece83ffc12fa9936449a785d87106ff105c
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-23 23:23:24 +0100

    Add SCENARIOS configuration with probabilities and shifts

---
+++ config/settings.py
@@ -0,0 +1,14 @@
+SCENARIOS = {
+    "Favorable": {
+        "probability": 0.20,
+        "shift_bps": -25
+    },
+    "Central": {
+        "probability": 0.60,
+        "shift_bps": 0
+    },
+    "Defavorable": {
+        "probability": 0.20,
+        "shift_bps": 25
+    }
+}
